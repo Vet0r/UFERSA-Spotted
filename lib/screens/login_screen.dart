f2a83dd943ae60363f8be77479e36d936bc564dc
Vitor Targino <vitortargino@MacBook-Air-de-Vitor.local>
2024-06-06 10:03:07 -0300

    resize de login

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:spotted_ufersa/resources/auth_methods.dart';
 import 'package:spotted_ufersa/responsive/mobile_screen_layout.dart';
 import 'package:spotted_ufersa/responsive/responsive_layout.dart';
@@ -7,7 +6,6 @@ import 'package:spotted_ufersa/responsive/web_screen_layout.dart';
 import 'package:spotted_ufersa/screens/forgot_password.dart';
 import 'package:spotted_ufersa/screens/signup_screen.dart';
 import 'package:spotted_ufersa/utils/colors.dart';
-import 'package:spotted_ufersa/utils/global_variable.dart';
 import 'package:spotted_ufersa/utils/utils.dart';
 import 'package:spotted_ufersa/widgets/text_field_input.dart';
 
@@ -60,7 +58,6 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       body: Stack(
         children: [
           Image.asset(
@@ -70,132 +67,132 @@ class _LoginScreenState extends State<LoginScreen> {
             width: double.infinity,
           ),
           SafeArea(
-            child: Container(
-              padding: MediaQuery.of(context).size.width > webScreenSize
-                  ? EdgeInsets.symmetric(
-                      horizontal: MediaQuery.of(context).size.width / 3)
-                  : const EdgeInsets.symmetric(horizontal: 32),
-              width: double.infinity,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Flexible(
-                    flex: 2,
-                    child: Container(),
-                  ),
-                  Image.asset(
-                    'assets/logo.png',
-                    height: MediaQuery.of(context).size.height * 0.10,
-                  ),
-                  Image.asset(
-                    'assets/logo_string.png',
-                    width: MediaQuery.of(context).size.width * 0.5,
-                    height: MediaQuery.of(context).size.height * 0.06,
-                  ),
-                  const SizedBox(
-                    height: 64,
-                  ),
-                  TextFieldInput(
-                    hintText: 'Email institucional',
-                    textInputType: TextInputType.emailAddress,
-                    textEditingController: _emailController,
-                  ),
-                  const SizedBox(
-                    height: 24,
-                  ),
-                  TextFieldInput(
-                    hintText: 'Senha',
-                    textInputType: TextInputType.text,
-                    textEditingController: _passwordController,
-                    isPass: true,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 8.0),
-                    child: Row(
-                      children: [
-                        GestureDetector(
-                          onTap: () => Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (context) => const ForgotPassScreen(),
+            child: SingleChildScrollView(
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 32),
+                width: MediaQuery.of(context).size.width,
+                height: MediaQuery.of(context).size.height * 0.95,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Flexible(
+                      flex: 2,
+                      child: Container(),
+                    ),
+                    Image.asset(
+                      'assets/logo.png',
+                      height: MediaQuery.of(context).size.height * 0.10,
+                    ),
+                    Image.asset(
+                      'assets/logo_string.png',
+                      width: MediaQuery.of(context).size.width * 0.5,
+                      height: MediaQuery.of(context).size.height * 0.06,
+                    ),
+                    const SizedBox(
+                      height: 64,
+                    ),
+                    TextFieldInput(
+                      hintText: 'Email institucional',
+                      textInputType: TextInputType.emailAddress,
+                      textEditingController: _emailController,
+                    ),
+                    const SizedBox(
+                      height: 24,
+                    ),
+                    TextFieldInput(
+                      hintText: 'Senha',
+                      textInputType: TextInputType.text,
+                      textEditingController: _passwordController,
+                      isPass: true,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 8.0),
+                      child: Row(
+                        children: [
+                          GestureDetector(
+                            onTap: () => Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => const ForgotPassScreen(),
+                              ),
                             ),
-                          ),
-                          child: Container(
-                            padding: const EdgeInsets.symmetric(vertical: 8),
-                            child: const Text(
-                              'Esqueci a senha',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
+                            child: Container(
+                              padding: const EdgeInsets.symmetric(vertical: 8),
+                              child: const Text(
+                                'Esqueci minha senha',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  InkWell(
-                    onTap: loginUser,
-                    child: Container(
-                      width: MediaQuery.of(context).size.width * 0.3,
-                      alignment: Alignment.center,
-                      padding: const EdgeInsets.symmetric(vertical: 12),
-                      decoration: const BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: gradient,
-                          begin: Alignment.centerLeft,
-                          end: Alignment.centerRight,
-                        ),
-                        borderRadius: BorderRadius.all(Radius.circular(80.0)),
+                        ],
                       ),
-                      child: !_isLoading
-                          ? const Text(
-                              'Entrar',
-                            )
-                          : const CircularProgressIndicator(
-                              color: primaryColor,
-                            ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 12,
-                  ),
-                  Flexible(
-                    flex: 2,
-                    child: Container(),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 15.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          padding: const EdgeInsets.symmetric(vertical: 8),
-                          child: const Text(
-                            'Não tem uma conta?',
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    InkWell(
+                      onTap: loginUser,
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 0.3,
+                        alignment: Alignment.center,
+                        padding: const EdgeInsets.symmetric(vertical: 12),
+                        decoration: const BoxDecoration(
+                          gradient: LinearGradient(
+                            colors: gradient,
+                            begin: Alignment.centerLeft,
+                            end: Alignment.centerRight,
                           ),
+                          borderRadius: BorderRadius.all(Radius.circular(80.0)),
                         ),
-                        GestureDetector(
-                          onTap: () => Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (context) => const SignupScreen(),
-                            ),
-                          ),
-                          child: Container(
+                        child: !_isLoading
+                            ? const Text(
+                                'Entrar',
+                              )
+                            : const CircularProgressIndicator(
+                                color: primaryColor,
+                              ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 12,
+                    ),
+                    Flexible(
+                      flex: 2,
+                      child: Container(),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 15.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
                             padding: const EdgeInsets.symmetric(vertical: 8),
                             child: const Text(
-                              ' inscreva-se',
-                              style: TextStyle(
-                                fontWeight: FontWeight.bold,
+                              'Não tem uma conta?',
+                            ),
+                          ),
+                          GestureDetector(
+                            onTap: () => Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => const SignupScreen(),
+                              ),
+                            ),
+                            child: Container(
+                              padding: const EdgeInsets.symmetric(vertical: 8),
+                              child: const Text(
+                                ' inscreva-se',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
